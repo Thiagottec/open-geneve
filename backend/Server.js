fc3a280d42cc2168d3dfbff302d726242b507bde
thiago.ttec <thiago.ttec@gmail.com>
2021-04-28 21:50:45 -0300

    final

---
+++ backend/Server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://openGeneve:AB9W6YH1fv8aPsfF@opengeneve.uzc71.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const app = express();
+app.use(express.json());
+app.use(studentRoutes);
+app.listen(3040, () => console.log("http://localhost:3040"));
